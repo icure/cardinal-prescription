58f5510aa33f75a8e92a1d6c7fef8e1945c9d1c5
aduchate <ad@icure.com>
2024-12-02 16:55:49 +0100

    Improve search

--- src/src/components/PrescribeMedicationsSearch.svelte
+++ src/src/components/PrescribeMedicationsSearch.svelte
@@ -1,87 +1,96 @@
 <script lang='ts'>
-	import { SearchIcn } from '../icons/index.svelte';
-	import type { MedicationType } from '../types/index.svelte';
-	import MedicationRow from './MedicationRow.svelte';
-	import AddMedicationModal from './AddMedicationModal.svelte';
-	import {initialiseSdk, searchMedications} from "../../lib/cardinal";
-	import {onMount} from "svelte";
-	import {Amp, AmpStatus, type PaginatedListIterator} from "@icure/cardinal-be-sam";
-	import InfiniteScroll from "./InfiniteScroll.svelte";
+    import {SearchIcn} from '../icons/index.svelte';
+    import type {MedicationType} from '../types/index.svelte';
+    import MedicationRow from './MedicationRow.svelte';
+    import AddMedicationModal from './AddMedicationModal.svelte';
+    import {initialiseSdk, searchMedications} from "../../lib/cardinal";
+    import {onMount} from "svelte";
+    import {Amp, AmpStatus, type PaginatedListIterator} from "@icure/cardinal-be-sam";
+    import InfiniteScroll from "./InfiniteScroll.svelte";
 
-	let sdk: any;
+    let sdk: any;
 
-	onMount(async () => {
-		sdk = await initialiseSdk()
-	});
+    onMount(async () => {
+        sdk = await initialiseSdk()
+    });
 
-	let searchQuery: string | undefined = $state();
-	let dropdownDisplayed: boolean = $derived(!!searchQuery);
-	let showModal = $state(false);
-	let displayedMedications: PaginatedListIterator<Amp> | undefined
-	let pages: MedicationType[][] = $state([]);
-	let newPages: MedicationType[][] = $state([]);
+    let searchQuery: string | undefined = $state();
+    let dropdownDisplayed: boolean = $derived(!!searchQuery);
+    let showModal = $state(false);
+    let displayedMedications: PaginatedListIterator<Amp> | undefined
+    let pages: MedicationType[][] = $state([]);
+    let newPages: MedicationType[][] = $state([]);
 
-	async function loadPage(medications: PaginatedListIterator<Amp>) {
-		const now = Date.now()
-		const twoYearsAgo = now - 2 * 365 * 24 * 3600 * 1000;
-		return (await medications.next(10)).flatMap((amp: Amp) => amp.to && amp.to < now ? [] : amp.ampps.filter((ampp) => {
-			return ampp.from && ampp.from < now && (!ampp.to || ampp.to > now) && ampp.status == AmpStatus.Authorized && ampp.commercializations?.some((c) => !!c.from && (!c.to || c.to > twoYearsAgo));
-		}).map((ampp) => ({
-			ampId: amp.id,
-			id: ampp.ctiExtended,
-			title: ampp.prescriptionName?.fr ?? ampp.abbreviatedName?.fr ?? amp.prescriptionName?.fr ?? amp.name?.fr ?? amp.abbreviatedName?.fr ?? '',
-			activeIngredient: amp.vmp?.vmpGroup?.name?.fr ?? '',
-			price: ampp?.exFactoryPrice ? `€${ampp.exFactoryPrice}` : ''
-		})));
-	}
+    async function loadPage(medications: PaginatedListIterator<Amp>, min: number, acc: MedicationType[] = []): Promise<MedicationType[]> {
+        const now = Date.now()
+        const twoYearsAgo = now - 2 * 365 * 24 * 3600 * 1000;
+        const page = (!(await medications.hasNext()) ? [] : await medications.next(min)).flatMap((amp: Amp) => amp.to && amp.to < now ? [] : amp.ampps.filter((ampp) => {
+            return ampp.from && ampp.from < now && (!ampp.to || ampp.to > now) && ampp.status == AmpStatus.Authorized && ampp.commercializations?.some((c) => !!c.from && (!c.to || c.to > twoYearsAgo));
+        }).map((ampp) => ({
+            ampId: amp.id,
+            id: ampp.ctiExtended,
+            title: ampp.prescriptionName?.fr ?? ampp.abbreviatedName?.fr ?? amp.prescriptionName?.fr ?? amp.name?.fr ?? amp.abbreviatedName?.fr ?? '',
+            activeIngredient: amp.vmp?.vmpGroup?.name?.fr ?? '',
+            price: ampp?.exFactoryPrice ? `€${ampp.exFactoryPrice}` : ''
+        })))
 
-	$effect(() => {
-		let q = searchQuery;
-		if (q && q.length >= 3) {
-			searchMedications(sdk, 'fr', q).then(async (medications) => {
-				displayedMedications = medications;
-				if (medications) {
-                    const firstPage = await loadPage(medications);
-                    pages = [firstPage]
-				}
-			});
-		}
-	});
+        return page.length == 0 || page.length + acc.length >= min ? [...acc, ...page] : await loadPage(medications, min, [...acc, ...page])
+    }
 
-	const loadMore = async () => {
-		if (displayedMedications) {
-			newPages = [loadPage(displayedMedications)]
-			pages = [...pages, ...newPages]
-		}
-	}
+    $effect(() => {
+        let q = searchQuery;
+        if (q && q.length >= 3) {
+            const cachedQuery = q
+            pages = []
+            setTimeout(() => {
+                if (cachedQuery === searchQuery) {
+                    searchMedications(sdk, 'fr', cachedQuery).then(async (medications) => {
+                        displayedMedications = medications;
+                        if (medications) {
+                            const firstPage = await loadPage(medications, 10);
+                            pages = [firstPage]
+                        }
+                    });
+                }
+            }, 100)
+
+        }
+    });
+
+    const loadMore = async () => {
+        if (displayedMedications) {
+            newPages = [await loadPage(displayedMedications, 10)]
+            pages = [...pages, ...newPages]
+        }
+    }
 
 
 </script>
 
 <div class='prescribeMedications'>
 
-	<div class:dropdownDisplayed class='prescribeMedications__search'>
-		<p class='prescribeMedications__search' >Prescribe medications:</p>
-		<label for='searchMedications' class='prescribeMedications__search__inputWrap'>
-			<input id='searchMedications' type='text' placeholder='Find the drug' bind:value={searchQuery} />
-			<SearchIcn />
-		</label>
-	</div>
-	{#if dropdownDisplayed}
-		<div class='prescribeMedications__dropdown'>
-			{#each pages as medicationPage}
-			{#each medicationPage as medication}
-				<MedicationRow {medication} onModifyPrescription={() => (showModal = true)} />
-			{/each}
-			{/each}
-			<InfiniteScroll
-				threshold={50}
-				loadMore={() => loadMore()} />
-		</div>
-	{/if}
+    <div class:dropdownDisplayed class='prescribeMedications__search'>
+        <p class='prescribeMedications__search'>Prescribe medications:</p>
+        <label for='searchMedications' class='prescribeMedications__search__inputWrap'>
+            <input id='searchMedications' type='text' placeholder='Find the drug' bind:value={searchQuery}/>
+            <SearchIcn/>
+        </label>
+    </div>
+    {#if dropdownDisplayed}
+        <div class='prescribeMedications__dropdown'>
+            {#each pages as medicationPage}
+                {#each medicationPage as medication}
+                    <MedicationRow {medication} onModifyPrescription={() => (showModal = true)}/>
+                {/each}
+            {/each}
+            <InfiniteScroll
+                    threshold={50}
+                    loadMore={() => loadMore()}/>
+        </div>
+    {/if}
 </div>
 
-<AddMedicationModal bind:showModal />
+<AddMedicationModal bind:showModal/>
 
 
 <style lang='scss'>
@@ -150,9 +159,9 @@
 
     &__dropdown {
       width: 100%;
-		height: auto;
-		max-height: 380px;
-		overflow-y: scroll;
+      height: auto;
+      max-height: 380px;
+      overflow-y: scroll;
       display: flex;
       flex-direction: column;
       align-items: flex-start;
